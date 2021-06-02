7ca5ac015a1ee98563ae68616b3e623f865b38c6
felipejsm <felipejsm41@gmail.com>
2021-06-02 01:12:18 -0300

    first commit

---
+++ src/main/java/com/nssp/aboutnothing/AboutNothingApplication.java
@@ -0,0 +1,13 @@
+package com.nssp.aboutnothing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AboutNothingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AboutNothingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nssp/aboutnothing/AboutNothingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nssp.aboutnothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutNothingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
