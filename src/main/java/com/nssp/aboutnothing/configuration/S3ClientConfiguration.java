215627e4bcc51a5a11799349c2df1cbda0a2c366
felipejsm <felipejsm41@gmail.com>
2021-06-09 22:46:45 -0300

    s3 sync and async client config

---
+++ src/main/java/com/nssp/aboutnothing/configuration/S3ClientConfiguration.java
@@ -0,0 +1,46 @@
+package com.nssp.aboutnothing.configuration;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
+
+import java.net.URI;
+
+@Configuration
+@Data
+public class S3ClientConfiguration {
+
+    @Value("${aws.uri}")
+    private URI uri;
+
+    @Value("${aws.s3.region}")
+    private Region region;
+
+    @Value("${aws.s3.bucket}")
+    private String bucket;
+
+    @Bean
+    public S3Client getClient() {
+        S3ClientBuilder s3ClientBuilder = S3Client.builder();
+        if(this.uri != null) {
+            s3ClientBuilder.endpointOverride(uri);
+        }
+        return s3ClientBuilder.region(this.region)
+                .build();
+    }
+
+    @Bean
+    public S3AsyncClient getAsyncClient() {
+        var s3AsyncClientBuilder = S3AsyncClient.builder();
+        if(this.uri != null) {
+            s3AsyncClientBuilder.endpointOverride(this.uri);
+        }
+        return s3AsyncClientBuilder.region(this.region)
+                .build();
+    }
+}
