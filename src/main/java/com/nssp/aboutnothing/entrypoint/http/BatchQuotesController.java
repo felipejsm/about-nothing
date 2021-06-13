e7b31a69ec16e0922343065032c3069256cf20c4
felipejsm <felipejsm41@gmail.com>
2021-06-13 16:17:47 -0300

    upload multiplos arquivos

--- src/main/java/com/nssp/aboutnothing/entrypoint/http/BatchQuotesController.java
+++ src/main/java/com/nssp/aboutnothing/entrypoint/http/BatchQuotesController.java
@@ -1,24 +1,24 @@
 package com.nssp.aboutnothing.entrypoint.http;
 
 import com.nssp.aboutnothing.data.model.Quote;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/batch_quotes")
 public class BatchQuotesController {
-    @PostMapping
-    public ResponseEntity<List<Quote>> post() {
-
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Quote> post(@RequestParam("files") List<MultipartFile> arquivos) {
+        System.out.println(arquivos);
         return null;
     }
     @GetMapping("/status/{id_status}")
     public ResponseEntity<Quote> get() {
+
         return null;
     }
 }
